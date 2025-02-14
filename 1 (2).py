e5a6ff5d53ad2f9c2db8faa635a4674a32777f6e
mkraajltp <mkraajltp@gmail.com>
2025-02-15 05:18:41 +0530

    1 (2).py

--- 1 (2).py
+++ 1 (2).py
@@ -8,8 +8,7 @@ st.write("Welcome to the Education Platform. Select a course below:")
 courses = {
     "Railway science foundation": "https://t.me/+Ja5IJyCmnMVkM2Vl",
     "CGL +  CHSL + MTS MAINS": "https://t.me/+DumAzg8tD3I5NDc1",
-    "Machine Learning Basics": "https://example.com/ml",
-    "Web Development": "https://example.com/webdev"
+    "Machine Learning Basics": "https://example.com/ml"
 }
 st.image("home.jpg", caption="Welcome to the Education Platform", use_column_width=True)
 
